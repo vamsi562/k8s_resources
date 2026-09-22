d4d9191cf52811d1fee675d18e346bf3fe92ba55
vamsi krishna <vamsi.562@gmail.com>
2026-09-22 21:17:23 +0530

    added setup shell

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ##docker setup
-sudo su -
 dnf -y install dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
